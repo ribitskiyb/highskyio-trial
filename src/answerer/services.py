3dc125cf1bbe5c85b72420fdc653e1126e7771bd
Bogdan Ribitskiy <this.bogdanr@gmail.com>
2025-09-16 21:39:47 +0300

    feat: use question's hash as the cache key

--- src/answerer/services.py
+++ src/answerer/services.py
@@ -1,3 +1,6 @@
+import hashlib
+from typing import cast
+
 from fastapi import HTTPException
 from pydantic import BaseModel
 from redis.asyncio import StrictRedis
@@ -14,8 +17,10 @@ async def get_answer(
     question: str,
     redis_client: StrictRedis,
 ) -> SolverResponse:
+    cache_key = _generate_cache_key(question)
+
     # Check cache first
-    cached_answer = await redis_client.get(question)
+    cached_answer = cast(bytes, await redis_client.get(cache_key)).decode("utf-8")
     if cached_answer:
         return SolverResponse(answer=cached_answer)
 
@@ -31,6 +36,14 @@ async def get_answer(
     string_result = str(result)
 
     # Cache the result for 60 seconds
-    await redis_client.setex(question, 60, string_result)
+    await redis_client.setex(
+        name=cache_key,
+        time=60,
+        value=string_result.encode("utf-8"),
+    )
 
     return SolverResponse(answer=string_result)
+
+
+def _generate_cache_key(question: str) -> str:
+    return hashlib.md5(question.encode("utf-8")).hexdigest()
